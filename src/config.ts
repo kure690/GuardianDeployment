17ac9d4b65f13db75f557e2eb5aef6ffc1329502
jtangpuz8 <jtangpuz@guardian.ph>
2025-09-29 16:18:17 +0800

    changed config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  // GUARDIAN_SERVER_URL: import.meta.env.VITE_GUARDIAN_SERVER_URL,
-  GUARDIAN_SERVER_URL: "http://localhost:3000",
+  GUARDIAN_SERVER_URL: import.meta.env.VITE_GUARDIAN_SERVER_URL,
+  // GUARDIAN_SERVER_URL: "http://localhost:3000",
   STREAM_APIKEY: import.meta.env.VITE_STREAM_APIKEY,
   GOOGLE_MAPS_API_KEY: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
 };
